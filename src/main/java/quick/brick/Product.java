9fbe77b6004f20ab7bbad6bba94030aab25d968a
dmr <brickdamik1996@gmail.com>
2021-11-03 01:22:28 +0300

    Initial commit

---
+++ src/main/java/quick/brick/Product.java
@@ -0,0 +1,23 @@
+package quick.brick;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class Product {
+
+    private final int id;
+    private final String title;
+    private final BigDecimal cost;
+
+    public Product(int id, String title, BigDecimal cost) {
+        this.id = id;
+        this.title = title;
+        this.cost = cost;
+    }
+
+    @Override
+    public String toString() {
+        return "id: " + id + "; Название: " + title +
+                "; Цена: " + cost.setScale(0, RoundingMode.CEILING) + "р;";
+    }
+}
